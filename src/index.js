429771aabca39802a7507ba10ed5d7befebc36dc
hidesuzuki <hide_suzuki@sharecrest.com>
2019-05-29 12:10:34 +0900

    Change:データをprops経由で渡す

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
 
 
-class Square extends React.Component {
+  class Square extends React.Component {
     render() {
       return (
-        <button className="square">
-          {/* TODO */}
+        <button className="square" onClick={function() {alert('Click');}}>
+          {this.props.value}
         </button>
       );
     }
@@ -24,7 +24,7 @@ class Square extends React.Component {
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
     }
   
     render() {
