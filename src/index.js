483df613b99f53edd590b297feada70c7d33dc84
hidesuzuki <hide_suzuki@sharecrest.com>
2019-05-29 12:38:09 +0900

    Change:Stateのリフトアップ
    Handleclick関数の実装

--- src/index.js
+++ src/index.js
@@ -32,6 +32,12 @@ serviceWorker.unregister();
       };
     }
 
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
     renderSquare(i) {
       return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
     }
