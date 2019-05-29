35d60fb1d43be6061869cd77ec90cacfed7e4379
hidesuzuki <hide_suzuki@sharecrest.com>
2019-05-29 13:13:42 +0900

    Change:過去の着手の表示

--- src/index.js
+++ src/index.js
@@ -77,8 +77,17 @@ serviceWorker.unregister();
     render() {
       const history = this.state.history;
       const current = history[history.length - 1];
-
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ? 'Go to move #' + move : 'Go to Game Start';
+        return (
+          <li>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+
       let status;
       if(winner){
         status = 'Winner:' + winner;
@@ -93,7 +102,7 @@ serviceWorker.unregister();
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
