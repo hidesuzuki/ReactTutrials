a8da14ca71c753e8d5374a1f074973e084cfaec8
hidesuzuki <hide_suzuki@sharecrest.com>
2019-05-29 12:47:34 +0900

    Change:手番の処理
    XとOが交互に入力できるように対応

--- src/index.js
+++ src/index.js
@@ -26,13 +26,17 @@ serviceWorker.unregister();
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
 
     handleClick(i){
       const squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({squares: squares});
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+        squares: squares,
+        xIsNext: !this.state.xIsNext
+      });
     }
 
     renderSquare(i) {
@@ -40,7 +44,7 @@ serviceWorker.unregister();
     }
   
     render() {
-      const status = 'Next player: X';
+      const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
   
       return (
         <div>
